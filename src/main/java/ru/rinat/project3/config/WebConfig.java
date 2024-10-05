02d2116c83a519e0ab1ee07cf1fad76c50f3b04a
rinat <mambetovpost@gmail.com>
2024-10-05 18:38:37 +0300

    Add Spring Data Paginator Config

---
+++ src/main/java/ru/rinat/project3/config/WebConfig.java
@@ -0,0 +1,23 @@
+package ru.rinat.project3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(PageRequest.of(0, 1000));
+        resolvers.add(resolver);
+    }
+
+}
